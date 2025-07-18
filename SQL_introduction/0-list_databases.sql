912180d26b0c2b04337daa3d8589d7abab5318c8
ydejene <y.dejene@alustudent.com>
2025-07-18 06:02:15 -0700

    Introduction to my sql

---
+++ SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- A script that lists all databases of MySQL server
+-- Query to display all databases
+SHOW DATABASES;
---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- A script that creates the database hbtn_0c_0 in MySQL server
+-- Create a database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
