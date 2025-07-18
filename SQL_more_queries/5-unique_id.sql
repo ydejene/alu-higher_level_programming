7db33b88683e07468d274f524d07e1dafe7406bc
ydejene <y.dejene@alustudent.com>
2025-07-18 06:52:50 -0700

    More on SQL QueryLanguage

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- A script that creates the table unique_id on your MySQL server
+-- Query that creates the table unique_id on your MySQL server
+CREATE TABLE IF NOT EXISTS unique_id (
+       id INT UNIQUE DEFAULT 1,
+       name VARCHAR(256));
+
