6689995cfb5b35d902e2ad4176de3499a09dd513
ydejene <y.dejene@alustudent.com>
2025-09-29 12:13:25 +0300

    updated the 1 question file

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (i = 0; i < words.length; i++) {
-  result = words[i];
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < words.length; i++) {
+  let result = words[i];
   console.log(result);
 }
