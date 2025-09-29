0559140895dab913461402b2e4ef9ac090313b2b
ydejene <y.dejene@alustudent.com>
2025-09-29 14:04:59 +0300

    num 12

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -8,6 +8,6 @@ const unique = [...new Set(myArgs)];
 if (unique.length < 2) {
   console.log(0);
 } else {
-  const sortedNum = unique.sort((a, b) => b - a); /*descending order */
+  const sortedNum = unique.sort((a, b) => b - a); /* descending order */
   console.log(sortedNum[1]);
 }
