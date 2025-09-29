c363fcd9505b240d862b0dbb37f8c03e14b60e2b
ydejene <y.dejene@alustudent.com>
2025-09-29 12:38:27 +0300

    6 and 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,9 +4,9 @@ function add (a, b) {
   if (isNaN(a) || isNaN(b) || myArgs.length < 2) {
     return 'isNaN';
   } else {
-    result = parseFloat(a) + parseFloat(b);
+    const result = parseFloat(a) + parseFloat(b);
     return result;
   }
 }
-test = add(myArgs[0], myArgs[1]);
+const test = add(myArgs[0], myArgs[1]);
 console.log(test);
