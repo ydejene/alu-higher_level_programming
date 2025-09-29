49fefb79061fd1c1b0a2b152318e5bbc6b13fe02
ydejene <y.dejene@alustudent.com>
2025-09-29 13:59:40 +0300

    num 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// changing the string array inputs to Integers
+const myArgs = process.argv.splice(2).map((n) => parseInt(n));
+// removing duplicates from the array number collection
+const unique = [...new Set(myArgs)];
+
+if (unique.length < 2) {
+  console.log(0);
+} else {
+  const sortedNum = unique.sort((a, b) => b - a); /*descending order */
+  console.log(sortedNum[1]);
+}
