d1d752766609c34fd894987c9744859876af5227
ydejene <y.dejene@alustudent.com>
2025-09-29 12:06:51 +0300

    fixed speling error

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,11 +3,9 @@ let myArgs = process.argv[2];
 let num = parseInt(myArgs);
 let word = "C is fun";
 if (isNaN(num)) {
-    console.log("Missing number of occurrences")
-}
-else{
-    for(i=1; i<=num; i++)
-    {
-        console.log(word)
-    }
+  console.log("Missing number of occurrences");
+} else {
+  for (i = 1; i <= num; i++) {
+    console.log(word);
+  }
 }
