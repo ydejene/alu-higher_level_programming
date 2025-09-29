b11c8807f8540e66c463bf4e4932d67cc05082ef
ydejene <y.dejene@alustudent.com>
2025-09-29 12:20:14 +0300

    changes

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < words.length; i++) {
-  let result = words[i];
+  const result = words[i];
   console.log(result);
 }
