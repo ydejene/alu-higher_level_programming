6929f96b4cddd9a71cbe0503c9d82d230d9120ea
ydejene <y.dejene@alustudent.com>
2025-09-29 14:26:47 +0300

    13 num

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
 // using the commonJS module system w/c is the default system used in server-side Node.js envts and is not natively supported in browsers
-module.exports = function add(a, b) {
+function add (a, b) {
   return parseInt(a) + parseInt(b);
-};
+}
+
+module.exports = { add };
