ae863cd32f6061372198b554a9983ae4398612c6
ydejene <y.dejene@alustudent.com>
2025-09-29 12:41:48 +0300

    fix 6 and 9

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArgs = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const myArgs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myArgs.length; i++) {
   console.log(myArgs[i]);
 }
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const myArgs = process.argv.splice(2);
 function add (a, b) {
   if (isNaN(a) || isNaN(b) || myArgs.length < 2) {
-    return 'isNaN';
+    return 'NaN';
   } else {
     const result = parseFloat(a) + parseFloat(b);
     return result;
