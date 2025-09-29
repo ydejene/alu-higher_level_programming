0b59aa8a50ca5a518f90190b3658928dc9c1483d
ydejene <y.dejene@alustudent.com>
2025-09-29 13:16:48 +0300

    num 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+function factorial(num) {
+    if (isNaN(num) || num === 0 || num === 1) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+const myArgs = process.argv[2];
+const inputNum = parseFloat(myArgs);
+const test = factorial(inputNum);
+console.log(test);
