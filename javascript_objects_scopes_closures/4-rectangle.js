5b292c28377a12941bf599202802fda1e0641863
ydejene <y.dejene@alustudent.com>
2025-10-02 10:45:26 +0300

    four

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,18 +1,26 @@
 #!/usr/bin/node
+
 module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    for (let i = 1; i <= this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   }
 
   rotate () {
+    // using the array destructuring method
     [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+    this.width *= 2;
+    this.height *= 2;
   }
-};
+}
