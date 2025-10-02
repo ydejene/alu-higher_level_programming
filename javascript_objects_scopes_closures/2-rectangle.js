284ffa5eb6e531d2a4cb9bc61d96e59047787cf0
ydejene <y.dejene@alustudent.com>
2025-10-02 10:25:58 +0300

    1 and 2 for the semistandard check

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,9 +3,8 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-      this.width = r;
+      this.width = w;
       this.height = h;
-    } else {
     }
   }
 };
