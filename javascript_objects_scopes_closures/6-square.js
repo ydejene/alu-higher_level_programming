82fc38da9a26382f0275599e00ab78b179e72458
ydejene <y.dejene@alustudent.com>
2025-10-02 11:18:30 +0300

    num 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
-  charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-      }
+
+const Square = require('5-square.js');
+
+module.exports = class Square extends Square {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i <= this.size; i++) {
+      console.log(c.repeat(this.size));
     }
   }
 };
