fda36e3436d909919ed6b1b7b11a618a09f4dbc5
ydejene <y.dejene@alustudent.com>
2025-10-02 12:16:17 +0300

    num 6 test

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,6 @@
 const ParentSquare = require('./5-square.js');
 
 module.exports = class Square extends ParentSquare {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 1; i <= this.height; i++) {
       console.log(c.repeat(this.width));
