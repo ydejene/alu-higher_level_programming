420ae36c15747fb80b0b1c2e76ce019aa6eb24cc
ydejene <y.dejene@alustudent.com>
2025-10-04 13:05:15 +0300

    num 8 semistandard check failed

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let reversedArray = [];
+  const reversedArray = [];
   for (let i = list.length - 1; i >= 0; i--) {
     reversedArray.push(list[i]);
   }
   return reversedArray;
 };
+
+
+/* if we were allowed to use reverse() method we could do:
+ exports.esrever = function (list) {
+ we are using slice to creates a shallow copy of list so the original
+ array remains unchanged (not modified)
+  return list.slice().reverse()
+}*/ 
