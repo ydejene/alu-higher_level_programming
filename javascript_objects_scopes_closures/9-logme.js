da2790edc7390279fdbf0f08999412bcb8ff6636
ydejene <y.dejene@alustudent.com>
2025-10-04 13:21:04 +0300

    num 9 forgot the increment

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -3,5 +3,6 @@
   let counter = 0; // declaring it outside the function so it won't reset value to zero when invoked rather persists
   exports.logMe = function (item) {
     console.log(`${counter}: ${item}`);
+    counter++;
   };
 }
