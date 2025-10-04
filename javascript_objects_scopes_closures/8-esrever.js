d168068e4a6f6b75fdd2e848e1c70f8c8c3f7209
ydejene <y.dejene@alustudent.com>
2025-10-04 13:19:24 +0300

    num 9

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -8,10 +8,9 @@ exports.esrever = function (list) {
   return reversedArray;
 };
 
-
 /* if we were allowed to use reverse() method we could do:
  exports.esrever = function (list) {
  we are using slice to creates a shallow copy of list so the original
  array remains unchanged (not modified)
   return list.slice().reverse()
-}*/ 
+} */
--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-let count = 0;
-exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
+{
+  let counter = 0; // declaring it outside the function so it won't reset value to zero when invoked rather persists
+  exports.logMe = function (item) {
+    console.log(`${counter}: ${item}`);
+  };
+}
