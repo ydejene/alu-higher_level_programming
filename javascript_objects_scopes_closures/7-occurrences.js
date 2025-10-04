1387c8452a14b478935827f715d5a609dc359062
ydejene <y.dejene@alustudent.com>
2025-10-04 12:39:29 +0300

    num 7 semistandard corrections

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-exports.nbOccurences = function (list, searchElement){
+exports.nbOccurences = function (list, searchElement) {
   let counter = 0;
   // we can use the for .. of to make a cleaner code
   for (const element of list) {
     // if condition to check searchElement === element if true update the counter
-    if (element === searchElement) counter++; 
+    if(element === searchElement) counter++; 
   }
   return counter;
-}
+};
